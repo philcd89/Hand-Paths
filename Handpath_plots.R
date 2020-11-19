65e24f2e2c582051f69aa7e75e015002ed22f36e
Phillip Desrochers <philcd89@gmail.com>
2020-11-19 13:12:51 -0500

    new commit

---
+++ Handpath_plots.R
@@ -0,0 +1,22 @@
+# This code reads in post-step 2 hand path data for cleaned KINARM trials and generates hand path plots using ggplot2.  This code was previously written in MATLAB.
+
+# Written by Phil Desrochers 11-19-2020.  Stored in github repository at 
+
+
+# Setup -------------------------------------------------------------------
+
+library(tidyverse)
+
+setwd("Z:/Data/Combined_Vis_Dyn/Kinematic Data/Post_Step_2")
+
+
+# Choose Single Participant or ALL participants ---------------------------
+
+# If user chooses to run all participants, this code will aggregate all of the post-step-2 files in the working directory.
+#oh hey
+
+
+# Read in Data ------------------------------------------------------------
+
+
+
